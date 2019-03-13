97bc4db46b6e056583506946b58631704835d076
vinnyA3 <vincent.aceto@gmail.com>
2019-03-13 15:32:38 -0400

    feat(seed-helpers): use indexes for doc ids, formatting

--- database/seed-helpers.js
+++ database/seed-helpers.js
@@ -6,37 +6,29 @@ const randomIntGenerator = (min, max) => {
   return Math.floor(Math.random() * (floorMax - ceilMin + 1)) + ceilMin;
 };
 
-const fakeComments = (() => {
-  const comments = [];
+const fakeComments = JSON.stringify(
+  (() => {
+    const comments = [];
 
-  for (let i = 0; i < 4; i += 1) {
-    comments.push({
-      body: faker.lorem.sentence(),
-      date: faker.date.between('2018-05-01', '2019-02-20'), // 2 review.date,
-      comment_id: randomIntGenerator(1, 500),
-      comment_author_name: faker.internet.userName(),
-    });
-  }
+    for (let i = 0; i < 4; i += 1) {
+      comments.push({
+        body: faker.lorem.sentence(),
+        date: faker.date.between('2018-05-01', '2019-02-20'), // 2 review.date,
+        comment_id: randomIntGenerator(1, 500),
+        comment_author_name: faker.internet.userName(),
+      });
+    }
 
-  return comments;
-})();
-
-// const buildFakeImages = n => {
-//   let count = n; // eslint-disable-line
-//   const imageUrls = [];
-
-//   while (n > 0) {
-//     imageUrls.push(faker.image.avatar());
-//     count -= 1;
-//   }
-
-//   return imageUrls;
-// };
+    return comments;
+  })()
+);
 
 const staticReviewData = (() => {
   return {
     author_id: randomIntGenerator(1, 500),
     author_name: faker.internet.userName(),
+    author_avatar:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/dudestein/128.jpg',
     body: faker.lorem.sentences(),
     likes: randomIntGenerator(0, 250),
   };
@@ -46,56 +38,75 @@ const reviewGenerator = index => {
   return Object.assign(
     {},
     {
-      // review_id: index,
+      review_id: index,
       date: faker.date.between('2018-05-01', '2019-02-20'), // 2 review.date,
       review_star_rating: randomIntGenerator(0, 5),
       review_item_rating: randomIntGenerator(1, 100),
-      comments: JSON.stringify(fakeComments),
+      comments: fakeComments, // static data
     },
     staticReviewData
   );
 };
 
 // eslint-disable-next-line
-const documentGenerator = () => {
-  // const imagePool = buildFakeImages(20);
+const documentGenerator = index => {
   return {
-    item_name: faker.commerce.productName(),
-    reviews: [...reviewGenerator(randomIntGenerator(0, 5))],
+    _id: index,
+    reviews: (() => {
+      const reviews = [];
+      const numberToGenerate = randomIntGenerator(1, 10);
+      for (let i = 0; i < numberToGenerate; i++) {
+        reviews.push(reviewGenerator(i));
+      }
+      return reviews;
+    })(), // inline iffe :: void 0 -> Array[Reviews]
   };
 };
 
-const seed = batchSize => {
-  const reviews = [];
+const seed = (idx = 0, batchSize) => {
+  const docs = [];
   for (let i = 0; i < batchSize; i += 1) {
-    reviews.push(reviewGenerator(i));
+    docs.push(documentGenerator(idx++));
   }
-  return reviews;
+  return docs;
 };
 
-const asyncSeed = async (db, collectionSize = 10000000, batchSize = 500000) => {
+const asyncSeed = async (db, collectionSize = 10000000, batchSize = 300000) => {
+  let entryIndex = 1;
+
   for (let i = 0; i < collectionSize; i += batchSize) {
     console.log('batching...', i); // eslint-disable-line
     console.log(process.memoryUsage()); // eslint-disable-line
 
     /* eslint-disable-next-line */
-    await db
-      .insertMany(seed(batchSize), { ordered: false })
-      .catch(e => console.error('err', e)); // eslint-disable-line
+    try {
+      await db.insertMany(seed(entryIndex, batchSize), { ordered: false });
+      entryIndex += batchSize; // increase the batch size
+    } catch (e) {
+      console.error(`An error has occurred: ${e}`);
+      process.exit(1);
+    }
   }
 };
 
-const asyncSeed1 = async (db, collectionSize = 10000000, batchSize = 500000, cs, pgp) => {
+// ugly postgres
+const asyncSeed1 = async (
+  db,
+  collectionSize = 10000000,
+  batchSize = 500000,
+  cs,
+  pgp
+) => {
   for (let i = 0; i < collectionSize; i += batchSize) {
     console.log('batching...', i); // eslint-disable-line
     console.log(process.memoryUsage()); // eslint-disable-line
 
     /* eslint-disable-next-line */
-    await db.none(pgp.helpers.insert(seed(batchSize), cs)) 
+    await db.none(pgp.helpers.insert(seed(batchSize), cs));
   }
 };
 
 module.exports = {
   asyncSeed,
-  asyncSeed1
+  asyncSeed1,
 };
